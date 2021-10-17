77262f3ec98ce37306cec77e05f2729883c60172
Name <email@example.ru>
2021-10-17 19:08:27 +1100

    Добавьте файлы проекта.

---
+++ Bot_Telegram_mode11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bot_Telegram_mode11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
